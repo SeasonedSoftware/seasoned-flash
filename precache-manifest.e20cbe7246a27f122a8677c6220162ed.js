75d153aeb52ccd79b8f415b800f22c3331c98b02
Gustavo Guichard <gustavoguichard@gmail.com>
2019-06-25 17:05:10 -0300

    Updates

---
+++ precache-manifest.e20cbe7246a27f122a8677c6220162ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cd147afe80432e3ec06f6b2ebdd9d75",
+    "url": "/seasoned-flash/index.html"
+  },
+  {
+    "revision": "59aee9b5a4433399150c",
+    "url": "/seasoned-flash/static/css/main.04f34a25.chunk.css"
+  },
+  {
+    "revision": "bb285d4e87395b46e481",
+    "url": "/seasoned-flash/static/js/2.a8736488.chunk.js"
+  },
+  {
+    "revision": "59aee9b5a4433399150c",
+    "url": "/seasoned-flash/static/js/main.6697348e.chunk.js"
+  },
+  {
+    "revision": "273887fa813da9bd5dcb",
+    "url": "/seasoned-flash/static/js/runtime~main.e78ac836.js"
+  }
+]);
